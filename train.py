e41d2a8d061d6874dab093d537d6d71f045e558f
aitests <56600226+aitests@users.noreply.github.com>
2019-10-22 13:43:06 +0300

    Update train.py

--- train.py
+++ train.py
@@ -160,7 +160,7 @@ def main(_argv):
                 'checkpoints/yolov3_train_{}.tf'.format(epoch))
     else:
         model.compile(optimizer=optimizer, loss=loss,
-                      run_eagerly=(FLAGS.mode == 'eager_fit'))
+                      run_eagerly=(FLAGS.mode == 'eager_fit'), metrics=['accuracy'])
 
         callbacks = [
             ReduceLROnPlateau(verbose=1),
